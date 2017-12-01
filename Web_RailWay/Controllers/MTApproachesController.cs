560b4dcdb3f9b76a9396d0ccfe5a7bf67c431c0e
ealevchenko <eduard.levchenko@arcelormittal.com>
2017-12-01 11:45:30 +0200

    add web MT

---
+++ Web_RailWay/Controllers/MTApproachesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_RailWay.Controllers
+{
+    public class MTApproachesController : Controller
+    {
+        // GET: MTApproaches
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
